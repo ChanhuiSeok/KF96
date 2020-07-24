726117dd36a89b473ef8019d372f0933776e533d
chanhuiseok <chanhuicom@gmail.com>
2020-07-24 19:23:33 +0900

    merge init

--- app/src/main/java/com/example/kf96_3/HomeFragment.java
+++ app/src/main/java/com/example/kf96_3/HomeFragment.java
@@ -30,49 +30,9 @@ import java.net.URLEncoder;
  */
 public class HomeFragment extends Fragment {
 
-    TextView cur_location;
-    TextView cur_dust;
-    XmlPullParser xpp;
-
-    String key = "CWiUwqUoaLsPMRKzSVdqs4QtbeSFBCsdkmhLm9wVhQZT9nJYIL8jQBR9U6uKyhGEZoQSU2v4Yeh2yijtE7JBwA%3D%3D";
-    String data;
 
     public HomeFragment() {
         // Required empty public constructor
-    }
-
-    // API로부터 데이터 받아오기
-    public String getXmlData() throws IOException {
-        StringBuffer buffer = new StringBuffer();
-        String test_tmX = "244148.546388";
-        String test_tmY = "412423.75772";
-        String oper_ver = "1.0";
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList");
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=CWiUwqUoaLsPMRKzSVdqs4QtbeSFBCsdkmhLm9wVhQZT9nJYIL8jQBR9U6uKyhGEZoQSU2v4Yeh2yijtE7JBwA%3D%3D"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("tmX","UTF-8") + "=" + URLEncoder.encode("244148.546388", "UTF-8")); /*TM측정방식 X좌표*/
-        urlBuilder.append("&" + URLEncoder.encode("tmY","UTF-8") + "=" + URLEncoder.encode("412423.75772", "UTF-8")); /*TM측정방식 Y좌표*/
-        urlBuilder.append("&" + URLEncoder.encode("ver","UTF-8") + "=" + URLEncoder.encode("1.0", "UTF-8"));
-
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        //System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-        //System.out.println(sb.toString());
-        return sb.toString();
     }
 
     /**
@@ -101,14 +61,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=null;//Fragment가 보여줄 View 객체를 참조할 참조변수
-        view= inflater.inflate(R.layout.fragment_home, container, false);
 
-        cur_location = (TextView) view.findViewById(R.id.cur_location);
-        cur_dust = (TextView) view.findViewById(R.id.cur_dust);
-
-
-
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
--- app/src/main/java/com/example/kf96_3/MainActivity.java
+++ app/src/main/java/com/example/kf96_3/MainActivity.java
@@ -4,24 +4,112 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     ChipNavigationBar bottomNav;
     FragmentManager fragmentManager;
+    HomeFragment homefrag;
+
+    // API로부터 데이터 받아오기
+    public ArrayList<String> getXmlData() throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        ArrayList<String> list = new ArrayList<String>();
+
+        String test_tmX = "244148.546388";
+        String test_tmY = "412423.75772";
+        String oper_ver = "1.0";
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getNearbyMsrstnList");
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=CWiUwqUoaLsPMRKzSVdqs4QtbeSFBCsdkmhLm9wVhQZT9nJYIL8jQBR9U6uKyhGEZoQSU2v4Yeh2yijtE7JBwA%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("tmX", "UTF-8") + "=" + URLEncoder.encode("244148.546388", "UTF-8")); /*TM측정방식 X좌표*/
+        urlBuilder.append("&" + URLEncoder.encode("tmY", "UTF-8") + "=" + URLEncoder.encode("412423.75772", "UTF-8")); /*TM측정방식 Y좌표*/
+        urlBuilder.append("&" + URLEncoder.encode("ver", "UTF-8") + "=" + URLEncoder.encode("1.0", "UTF-8"));
+
+        try {
+            URL url = new URL(urlBuilder.toString());
+        /*
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        //System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();*/
+            BufferedInputStream bis = new BufferedInputStream(url.openStream());
+            String tag = null, location = null;
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();//xml파싱을 위한
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(bis, "UTF-8")); //inputstream 으로부터 xml 입력받기
+
+            int event_type= xpp.getEventType();
+            while (event_type != XmlPullParser.END_DOCUMENT) {
+                if (event_type == XmlPullParser.START_TAG) {
+                    tag = xpp.getName();
+                } else if (event_type == XmlPullParser.TEXT) {
+                    if(tag.equals("stationName")){
+                        location = xpp.getText();
+                    }
+                } else if (event_type == XmlPullParser.END_TAG) {
+                    tag = xpp.getName();
+                    if (tag.equals("item")) {
+                        list.add(location);
+                    }
+                }
+                event_type = xpp.next();
+            }
+
+        } catch (Exception e) {
+
+        }
+
+        return list;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bottomNav = findViewById(R.id.bottom_nav);
 
-        if (savedInstanceState == null){
+        
+
+
+        if (savedInstanceState == null) {
             bottomNav.setItemSelected(R.id.home, true);
             fragmentManager = getSupportFragmentManager();
             HomeFragment homeFrag = new HomeFragment();
@@ -34,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(int id) {
                 Fragment fragment = null;
-                switch (id){
+                switch (id) {
                     case R.id.home:
                         fragment = new HomeFragment();
                         break;
@@ -49,12 +137,12 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
 
-                if (fragment != null){
+                if (fragment != null) {
                     fragmentManager = getSupportFragmentManager();
                     fragmentManager.beginTransaction()
                             .replace(R.id.fragment_container, fragment)
                             .commit();
-                }else {
+                } else {
                     Log.e(TAG, "Error in creating fragment");
                 }
             }
